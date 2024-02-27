b8205ea828565ff2cf0ad82d73138195f26ab1ca
aaidilz <muhaidfik@gmail.com>
2024-02-27 14:50:52 +0700

    tagas3 selesai

--- tugas/tugas3/3b.php
+++ tugas/tugas3/3b.php
@@ -1,20 +1,10 @@
 <?php
 
 function number($num){
-    $count = 1;
+    $jumlah = 1;
     for ($i = 1; $i <= $num; $i++) {
         for ($j = 1; $j <= $i; $j++) {
-            echo $count++ . " ";
-        }
-        echo "<br>";
-    }
-}
-
-//buat $post agar bisa diakses di dalam function
-function random($num){
-    for ($i = 1; $i <= $num; $i++) {
-        for ($j = 1; $j <= $i; $j++) {
-            echo rand(1, 100) . " ";
+            echo $jumlah++ . " ";
         }
         echo "<br>";
     }
